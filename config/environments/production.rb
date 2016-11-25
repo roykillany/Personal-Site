24ec0554f9af7c9bef1ab16890427e5e45fb8cc1
Anthony Chang <a.chang@celmatix.com>
2016-11-25 16:26:25 -0500

    Update variables for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  Paperclip.options[:command_path] = "/usr/bin/convert"
+  Paperclip.options[:log] = true
+  Paperclip.options[:log_command] = true
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV['S3_BUCKET'],
+      access_key_id: ENV['S3_ACCESS_KEY_ID'],
+      secret_access_key: ENV['S3_SECRET_ACCESS_KEY']
+    }
+  }
 end
