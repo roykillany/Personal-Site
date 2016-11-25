5179e66799a90765d9aa1a62b705c816fc2a63f3
Anthony Chang <a.chang@celmatix.com>
2016-11-25 11:41:00 -0500

    Add bucket to s3

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ActiveRecord::Base
 	has_attached_file :image,
 		styles: { medium: "300x300>", thumb: "100x100>", tiny: "40x40" },
-		storage: :s3, default_url: "http://www.genengnews.com/app_themes/genconnect/images/default_profile.jpg"
+		storage: :s3, bucket: :mumsiteprod, default_url: "http://www.genengnews.com/app_themes/genconnect/images/default_profile.jpg"
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
